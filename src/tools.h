71e660c6ce5a005d43f496d461b9ac667391f371
Сергей <31417895+immortalserg@users.noreply.github.com>
2023-06-05 16:23:23 +0200

    Delete src directory

--- src/tools.h
+++
@@ -1,9 +0,0 @@
-#ifndef ___TOOLS_H
-#define ___TOOLS_H
-
-#define LOG_FILE "/dev/null"
-//#define LOG_FILE "./log.txt"
-void lprintf(const char *format, ...);
-int print_help();
-
-#endif // ___TOOLS_H
